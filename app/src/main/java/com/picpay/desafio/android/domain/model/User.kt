852eb4071a37b546ccfe96b902507160d89653ec
Ingrid Silva <ingridsilvatec@outlook.com>
2025-02-11 18:25:02 -0300

    add user model

--- app/src/main/java/com/picpay/desafio/android/domain/model/User.kt
+++ app/src/main/java/com/picpay/desafio/android/domain/model/User.kt
@@ -2,7 +2,7 @@ package com.picpay.desafio.android.domain.model
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class User(
