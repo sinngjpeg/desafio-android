ff5384a005c5f414832958c2550f377c952027e7
Ingrid Silva <ingridsilvatec@outlook.com>
2025-02-10 21:58:41 -0300

    add initial network module configs

--- app/src/main/java/com/picpay/desafio/android/framework/di/NetworkModule.kt
+++ app/src/main/java/com/picpay/desafio/android/framework/di/NetworkModule.kt
@@ -1,3 +1,60 @@
 package com.picpay.desafio.android.framework.di
 
-class NetworkModule
+import com.picpay.desafio.android.BuildConfig
+import com.picpay.desafio.android.framework.network.PicPayService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val TIMEOUT_SECONDS = 15L
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            setLevel(
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else HttpLoggingInterceptor.Level.NONE
+            )
+        }
+    }
+
+    @Provides
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    fun providesRetrofit(
+        okHttpClient: OkHttpClient,
+        converterFactory: GsonConverterFactory
+    ): PicPayService {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(converterFactory)
+            .build()
+            .create(PicPayService::class.java)
+    }
+}
