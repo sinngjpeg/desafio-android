1d1d5ab60d7459f210c21a9cba35cba1d93e8ee9
Ingrid Silva <ingridsilvatec@outlook.com>
2025-02-11 23:07:09 -0300

    add initial config to detail fragment

--- app/src/main/java/com/picpay/desafio/android/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/picpay/desafio/android/presentation/detail/DetailFragment.kt
@@ -6,13 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.picpay.desafio.android.R
+import com.picpay.desafio.android.databinding.FragmentDetailBinding
+import com.picpay.desafio.android.databinding.FragmentUserBinding
 
 class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding: FragmentDetailBinding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
-    }
+    ) = FragmentDetailBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
+
 }
