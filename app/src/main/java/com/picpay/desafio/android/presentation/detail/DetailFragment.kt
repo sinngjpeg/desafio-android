abdfaa3ba64db1be2e5416f6129b23d2b1caa840
Ingrid Silva <ingridsilvatec@outlook.com>
2025-02-11 21:52:52 -0300

    add new icon to app

--- app/src/main/java/com/picpay/desafio/android/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/picpay/desafio/android/presentation/detail/DetailFragment.kt
@@ -1,15 +1,16 @@
 package com.picpay.desafio.android.presentation.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.picpay.desafio.android.R
 
 class DetailFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
--- app/src/main/java/com/picpay/desafio/android/presentation/user/UserFragment.kt
+++ app/src/main/java/com/picpay/desafio/android/presentation/user/UserFragment.kt
@@ -1,17 +1,18 @@
 package com.picpay.desafio.android.presentation.user
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.picpay.desafio.android.R
 
 
 class UserFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_user, container, false)
