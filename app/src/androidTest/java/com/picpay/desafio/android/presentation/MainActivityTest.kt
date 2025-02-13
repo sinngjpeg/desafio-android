01f0d864d16d051f05d1ad96c1e1bb54ddcdd972
Ingrid Silva <ingridsilvatec@outlook.com>
2025-02-13 20:29:54 -0300

    fix main activity test

--- app/src/androidTest/java/com/picpay/desafio/android/presentation/MainActivityTest.kt
+++ app/src/androidTest/java/com/picpay/desafio/android/presentation/MainActivityTest.kt
@@ -2,12 +2,13 @@ package com.picpay.desafio.android.presentation
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
 import com.picpay.desafio.android.R
 import com.picpay.desafio.android.presentation.user.UserFragment
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,22 +18,23 @@ import org.junit.runner.RunWith
 class MainActivityTest {
 
     @get:Rule
-    val activityRule = ActivityTestRule(MainActivity::class.java)
+    val hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
 
     @Test
     fun testActivityRecreation() {
         val scenario = ActivityScenario.launch(MainActivity::class.java)
 
         scenario.onActivity { activity ->
-            // Verificar estado inicial ou configuração inicial
             assertNotNull(activity.supportFragmentManager.findFragmentById(R.id.nav_host_fragment))
         }
 
-        // Recriar a atividade
         scenario.recreate()
-
         scenario.onActivity { activity ->
-            // Verificar novamente após recriação
             assertNotNull(activity.supportFragmentManager.findFragmentById(R.id.nav_host_fragment))
         }
     }
@@ -42,10 +44,7 @@ class MainActivityTest {
         val scenario = ActivityScenario.launch(MainActivity::class.java)
 
         scenario.onActivity { activity ->
-            // Obter o NavHostFragment
             val navHostFragment = activity.supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-
-            // Verificar se o fragment inicial foi carregado
             val currentFragment = navHostFragment?.childFragmentManager?.fragments?.firstOrNull()
             assertTrue(currentFragment is UserFragment)
         }
